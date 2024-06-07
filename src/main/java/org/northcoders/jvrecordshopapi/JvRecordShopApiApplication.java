94b8cda0ff9a534aa7329edd29183a28cbf73f84
Elliot <elliotgj@GodburPC>
2024-06-07 14:45:27 +0100

    inital commit

---
+++ src/main/java/org/northcoders/jvrecordshopapi/JvRecordShopApiApplication.java
@@ -0,0 +1,29 @@
+package org.northcoders.jvrecordshopapi;
+
+import org.h2.tools.Server;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.sql.SQLException;
+
+@SpringBootApplication
+public class JvRecordShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JvRecordShopApiApplication.class, args);
+    }
+
+
+
+    /**
+     * Start internal H2 server so we can query the DB from IDE
+     *
+     * @return H2 Server instance
+     * @throws SQLException
+     */
+    @Bean(initMethod = "start", destroyMethod = "stop")
+    public Server h2Server() throws SQLException {
+        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "9092");
+    }
+}
---
+++ src/test/java/org/northcoders/jvrecordshopapi/JvRecordShopApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.northcoders.jvrecordshopapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JvRecordShopApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
