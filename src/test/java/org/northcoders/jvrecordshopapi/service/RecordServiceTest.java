2456a00831f68406b694819ea1b1355cec0aedd6
Elliot <elliotgj@GodburPC>
2024-06-07 17:57:47 +0100

    refactored setup

--- src/test/java/org/northcoders/jvrecordshopapi/service/RecordServiceTest.java
+++ src/test/java/org/northcoders/jvrecordshopapi/service/RecordServiceTest.java
@@ -1,14 +1,21 @@
 package org.northcoders.jvrecordshopapi.service;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.northcoders.jvrecordshopapi.model.Genre;
 import org.northcoders.jvrecordshopapi.model.Record;
+import org.northcoders.jvrecordshopapi.model.Stock;
+import org.northcoders.jvrecordshopapi.repository.GenreRepository;
 import org.northcoders.jvrecordshopapi.repository.RecordRepository;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.time.Year;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,29 +24,53 @@ import static org.mockito.BDDMockito.given;
 @DataJpaTest
 class RecordServiceTest {
 
-    List<Record> testRecords;
+    private List<Record> testRecords;
+
+    private Genre metal;
+    private HashSet<Genre> genres;
 
     @Mock
     private RecordRepository recordRepository;
+    @Mock
+    private GenreRepository genreRepository;
 
     @InjectMocks
     RecordService recordService;
 
     @BeforeEach
     void setup() {
+
         testRecords = new ArrayList<>();
-        Record record1 = new Record(); record1.setName("Test record 1"); testRecords.add(record1);
-        Record record2 = new Record(); record2.setName("Test record 2"); testRecords.add(record2);
-        Record record3 = new Record(); record3.setName("Test record 3"); testRecords.add(record3);
+        metal = new Genre(1L, "Metal", new HashSet<>());
+        genres = new HashSet<>(List.of(metal));
+        Record record1 = new Record(1L, "Record One", new ArrayList<>(),
+                Year.of(2022), new HashSet<>(), new Stock());
+
+        Record record2 = new Record(2L, "Record Two", new ArrayList<>(),
+                Year.of(2016), genres, new Stock());
+
+        Record record3 = new Record(3L, "Record Three", new ArrayList<>(),
+                Year.of(2020), genres, new Stock());
+        metal.setRecords(new HashSet<>(Arrays.asList(record1, record2)));
     }
 
 
 
     @Test
-    void getAllRecords() {
+    @DisplayName("Return all records")
+    void getAllRecordsTest() {
         given(recordRepository.findAll()).willReturn(testRecords);
         List<Record> results = recordService.getAllRecords();
         assertThat(results.size()).isEqualTo(testRecords.size());
         assertThat(results).isEqualTo(testRecords);
+    }
+
+    @Test
+    @DisplayName("Return all records in genre")
+    void getAllRecordsInGenreTest() {
+        given(genreRepository.findByName("Metal")).willReturn(metal);
+        List<Record> results = recordService.getAllRecordsGenre("Metal");
+        assertThat(results.size()).isEqualTo(metal.getRecords().size());
+        assertThat(results.get(0).getGenres()).isEqualTo(genres);
     }
 }
