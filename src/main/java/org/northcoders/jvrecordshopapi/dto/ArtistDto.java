a499b5b1217f5bcff96dae8163428834162f25bf
Elliot <elliotgj@GodburPC>
2024-06-11 15:57:05 +0100

    init ArtistDto

---
+++ src/main/java/org/northcoders/jvrecordshopapi/dto/ArtistDto.java
@@ -0,0 +1,11 @@
+package org.northcoders.jvrecordshopapi.dto;
+
+import java.util.List;
+
+public record ArtistDto(
+        long id,
+        String name,
+        List<RecordDto> records
+        )
+{
+}
